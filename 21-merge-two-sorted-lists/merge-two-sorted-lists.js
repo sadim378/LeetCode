697ff8656f564684854b08a85be78f4e77266795
Md. Abu Taleb Sadim <sadimakon3@gmail.com>
2025-10-16 17:47:21 +0600

    Time: 0 ms (100.00%) | Memory: 58.2 MB (20.64%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    var dummy = new ListNode(-1);
+    var current = dummy;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+    if (list1 !== null) current.next = list1;
+    if (list2 !== null) current.next = list2;
+
+    return dummy.next;
+};
