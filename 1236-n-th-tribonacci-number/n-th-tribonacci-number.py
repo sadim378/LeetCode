8279bfabe16d81bce5a956227e04a6555746b17b
Md. Abu Taleb Sadim <sadimakon3@gmail.com>
2026-08-08 13:53:30 +0600

    Time: 0 ms (100.00%) | Memory: 12.2 MB (90.81%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        if n == 0:
+            return 0
+
+        if n == 1 or n == 2:
+            return 1
+
+        a = 0
+        b = 1
+        c = 1
+
+        for i in range(3, n + 1):
+            d = a + b + c
+
+            a = b
+            b = c
+            c = d
+
+        return c
